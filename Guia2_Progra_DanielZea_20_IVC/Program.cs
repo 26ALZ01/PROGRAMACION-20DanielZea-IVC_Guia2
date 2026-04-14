e0b4b332903f42bc122be7c0b40901ee47e4eef8
26ALZ01-DanielZea <lopezalvaro16db@gmail.com>
2026-04-13 18:18:34 -0600

    Echo en clase

---
+++ Guia2_Progra_DanielZea_20_IVC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string l;
+
+        // Interfaz de usuario
+        Console.WriteLine("*** MENÚ DE CALIFICACIONES ***");
+        Console.Write("Ingrese una letra (A, B, C, D o F): ");
+
+        // Leemos la entrada y la convertimos a mayúsculas de una vez
+        l = Console.ReadLine().ToUpper();
+
+        // Estructura Switch
+        switch (l)
+        {
+            case "A":
+                Console.WriteLine("Resultado: Excelente");
+                break;
+            case "B":
+                Console.WriteLine("Resultado: Bueno");
+                break;
+            case "C":
+                Console.WriteLine("Resultado: Regular");
+                break;
+            case "D":
+                Console.WriteLine("Resultado: Deficiente");
+                break;
+            case "F":
+                Console.WriteLine("Resultado: Reprobado");
+                break;
+            default:
+                Console.WriteLine("Error: Letra no válida.");
+                break;
+        }
+
+        // Esperar a que el usuario presione una tecla para cerrar
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
